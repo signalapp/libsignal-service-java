14215c38e156d8599bf26c69f99987c572269fbe
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2017-05-11 12:27:27 +0200

    fix unnecessary threadpool-creation
    New OkHttpClients came with their own thread and connection pools. With newBuilder() we can create a new client with an already existing pool.
    
    Fixes https://github.com/WhisperSystems/Signal-Android/issues/6283

--- java/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ java/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -77,6 +77,7 @@ import okhttp3.Response;
 public class PushServiceSocket {
 
   private static final String TAG = PushServiceSocket.class.getSimpleName();
+  private static final OkHttpClient CLIENT = new OkHttpClient();
 
   private static final String CREATE_ACCOUNT_SMS_PATH   = "/v1/accounts/sms/code/%s";
   private static final String CREATE_ACCOUNT_VOICE_PATH = "/v1/accounts/voice/code/%s";
@@ -612,7 +613,7 @@ public class PushServiceSocket {
       SSLContext context = SSLContext.getInstance("TLS");
       context.init(null, trustManagers, null);
 
-      OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
+      OkHttpClient.Builder okHttpClientBuilder = CLIENT.newBuilder()
           .sslSocketFactory(context.getSocketFactory(), (X509TrustManager)trustManagers[0])
           .connectTimeout(soTimeoutMillis, TimeUnit.MILLISECONDS)
           .readTimeout(soTimeoutMillis, TimeUnit.MILLISECONDS);
