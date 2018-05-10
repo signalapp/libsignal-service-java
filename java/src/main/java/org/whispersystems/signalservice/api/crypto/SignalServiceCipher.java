8533a5100f63e333dad3b4d0d6c6b5ed02221b49
Greyson Parrelli <greyson.parrelli@gmail.com>
2018-05-09 22:59:28 -0700

    Fix contact serialization and deserialization.

--- java/src/main/java/org/whispersystems/signalservice/api/crypto/SignalServiceCipher.java
+++ java/src/main/java/org/whispersystems/signalservice/api/crypto/SignalServiceCipher.java
@@ -332,58 +332,56 @@ public class SignalServiceCipher {
                                                          .setType(type)
                                                          .build());
         }
-
-        if (contact.getNumberCount() > 0) {
-          for (DataMessage.Contact.Phone phone : contact.getNumberList()) {
-            SharedContact.Phone.Type type = SharedContact.Phone.Type.HOME;
-
-            switch (phone.getType()) {
-              case HOME:   type = SharedContact.Phone.Type.HOME;   break;
-              case WORK:   type = SharedContact.Phone.Type.WORK;   break;
-              case MOBILE: type = SharedContact.Phone.Type.MOBILE; break;
-              case CUSTOM: type = SharedContact.Phone.Type.CUSTOM; break;
-            }
-
-            builder.withPhone(SharedContact.Phone.newBuilder()
-                                                 .setLabel(phone.getLabel())
-                                                 .setType(type)
-                                                 .setValue(phone.getValue())
-                                                 .build());
-          }
-
-          if (contact.getEmailCount() > 0) {
-            for (DataMessage.Contact.Email email : contact.getEmailList()) {
-              SharedContact.Email.Type type = SharedContact.Email.Type.HOME;
-
-              switch (email.getType()) {
-                case HOME:   type = SharedContact.Email.Type.HOME;   break;
-                case WORK:   type = SharedContact.Email.Type.WORK;   break;
-                case MOBILE: type = SharedContact.Email.Type.MOBILE; break;
-                case CUSTOM: type = SharedContact.Email.Type.CUSTOM; break;
-              }
-
-              builder.withEmail(SharedContact.Email.newBuilder()
-                                                   .setLabel(email.getLabel())
-                                                   .setType(type)
-                                                   .setValue(email.getValue())
-                                                   .build());
-            }
-          }
-
-          if (contact.hasAvatar()) {
-            builder.setAvatar(SharedContact.Avatar.newBuilder()
-                                                  .withAttachment(createAttachmentPointer(envelope, contact.getAvatar().getAvatar()))
-                                                  .withProfileFlag(contact.getAvatar().getIsProfile())
-                                                  .build());
-          }
-
-          if (contact.hasOrganization()) {
-            builder.withOrganization(contact.getOrganization());
-          }
-        }
-
       }
 
+      if (contact.getNumberCount() > 0) {
+        for (DataMessage.Contact.Phone phone : contact.getNumberList()) {
+          SharedContact.Phone.Type type = SharedContact.Phone.Type.HOME;
+
+          switch (phone.getType()) {
+            case HOME:   type = SharedContact.Phone.Type.HOME;   break;
+            case WORK:   type = SharedContact.Phone.Type.WORK;   break;
+            case MOBILE: type = SharedContact.Phone.Type.MOBILE; break;
+            case CUSTOM: type = SharedContact.Phone.Type.CUSTOM; break;
+          }
+
+          builder.withPhone(SharedContact.Phone.newBuilder()
+                                               .setLabel(phone.getLabel())
+                                               .setType(type)
+                                               .setValue(phone.getValue())
+                                               .build());
+        }
+      }
+
+      if (contact.getEmailCount() > 0) {
+        for (DataMessage.Contact.Email email : contact.getEmailList()) {
+          SharedContact.Email.Type type = SharedContact.Email.Type.HOME;
+
+          switch (email.getType()) {
+            case HOME:   type = SharedContact.Email.Type.HOME;   break;
+            case WORK:   type = SharedContact.Email.Type.WORK;   break;
+            case MOBILE: type = SharedContact.Email.Type.MOBILE; break;
+            case CUSTOM: type = SharedContact.Email.Type.CUSTOM; break;
+          }
+
+          builder.withEmail(SharedContact.Email.newBuilder()
+                                               .setLabel(email.getLabel())
+                                               .setType(type)
+                                               .setValue(email.getValue())
+                                               .build());
+        }
+      }
+
+      if (contact.hasAvatar()) {
+        builder.setAvatar(SharedContact.Avatar.newBuilder()
+                                              .withAttachment(createAttachmentPointer(envelope, contact.getAvatar().getAvatar()))
+                                              .withProfileFlag(contact.getAvatar().getIsProfile())
+                                              .build());
+      }
+
+      if (contact.hasOrganization()) {
+        builder.withOrganization(contact.getOrganization());
+      }
 
       results.add(builder.build());
     }
