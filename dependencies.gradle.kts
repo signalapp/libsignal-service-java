003e987b1ed3d97f56a404987c766ab54ea31656
AsamK <asamk@gmx.de>
2023-12-08 00:35:05 +0100

    Update libsignal-client to 0.35.0

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,7 @@
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      version("libsignal-client", "0.32.1")
+      version("libsignal-client", "0.35.0")
 
       // Kotlin
       version("kotlin", "1.8.10")
