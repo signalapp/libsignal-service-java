c52eac60944a0c08f514db11b1045292cd4dcb88
Alex Konradi <akonradi@signal.org>
2024-03-01 14:45:37 -0500

    [Signal-Android] Add libsignal-net CDSI implementation. 56eae8c7bff7af255f9a0bf30ef5847497510af5

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,7 @@
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      version("libsignal-client", "0.39.2")
+      version("libsignal-client", "0.40.1")
 
       // Kotlin
       version("kotlin", "1.8.10")
